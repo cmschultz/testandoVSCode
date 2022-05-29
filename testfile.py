1b350e855f3931da92aedbdda7814ebbe8f8360f
cmschultz <caioschultz@unifei.edu.br>
2022-05-29 14:30:00 -0300

    .

--- testfile.py
+++ testfile.py
@@ -44,5 +44,5 @@ def controleDistancia(dist):
     
 #Problema: Muitas variações de comando em pouco tempo.
 #talvez mexer com ranges de valores. Por exemplo: de 1 a 1,5 metros
-#velociadade relativa de até 10 km/h.
+#velociadade relativa de até 5 km/h.
 #Essa versão aqui é a Beta.
